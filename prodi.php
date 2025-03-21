acd11b5af1f1333caa099a308a37e9311e2064da
abdillahfajar35 <abdillahfajar72@gmail.com>
2025-03-21 11:37:46 +0800

    Read Data

--- prodi.php
+++ prodi.php
@@ -7,5 +7,27 @@
 </head>
 <body>
     <h1>Data Prodi</h1>
+    <br>
+    <table border ="1" cellspacing="0" cellpading="5">
+        <thead>
+            <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Kaprodi</th>
+            </tr>
+        </thead>
+        
+        <tbody>
+    <?php
+    $i = 1;
+    foreach ($data as $d) : ?>
+        <tr>
+            <td> <?php echo $i++; ?> </td>
+            <td> <?php echo $d["nama"] ?> </td>
+            <td> <?php echo $d["kaprodi"] ?> </td>
+        </tr>   
+    <?php endforeach; ?>
+
+</tbody> 
 </body>
 </html>
