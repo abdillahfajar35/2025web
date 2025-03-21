d846c83bc63ace9f1d6902c843d80fee027b36dd
abdillahfajar35 <abdillahfajar72@gmail.com>
2025-03-21 11:43:29 +0800

    Read Data

--- prodi.php
+++ prodi.php
@@ -1,9 +1,17 @@
+<?php
+include "koneksi.php";
+
+$query = "SELECT * FROM prodi";
+$data = ambildata($query);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SIMPADU POLIBAN</title>
+    <title>PRODI</title>
 </head>
 <body>
     <h1>Data Prodi</h1>
@@ -14,6 +22,7 @@
             <th>No</th>
             <th>Nama</th>
             <th>Kaprodi</th>
+            <th>Jurusan</th>
             </tr>
         </thead>
         
